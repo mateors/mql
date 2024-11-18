184edb208f75f3dbcde3ec5030b58f725f7b79a9
Mostain Billah <bill.rassel@gmail.com>
2024-11-18 22:09:18 +0600

    DBC added

--- database/database.go
+++ database/database.go
@@ -2,6 +2,8 @@ package database
 
 import "database/sql"
 
+var DBC *sql.DB
+
 type DB interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	Exec(query string, args ...interface{}) (sql.Result, error)
