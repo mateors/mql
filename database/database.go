7988c2df1199d3ceac328662c844a7ce863e1a50
Mostain Billah <bill.rassel@gmail.com>
2024-11-19 14:12:30 +0600

    code refactored

--- database/database.go
+++ database/database.go
@@ -2,8 +2,6 @@ package database
 
 import "database/sql"
 
-var DBC *sql.DB
-
 type DB interface {
 	Query(query string, args ...interface{}) (*sql.Rows, error)
 	Exec(query string, args ...interface{}) (sql.Result, error)
